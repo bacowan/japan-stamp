4b53eb96f99df4c4c767adf292045ddccf7b613a
bacowan <bacowan@ualberta.ca>
2024-07-23 23:35:19 +0900

    next.config.mjs wasn't configured properly

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en-US', 'jp'],
-    defaultLocale: 'en-US',
-  },
-    compiler: {
-        styledComponents: true,
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'leafletjs.com',
+          port: '',
+          pathname: '/docs/images/**',
+        },
+      ],
     },
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'leafletjs.com',
-            port: '',
-            pathname: '/docs/images/**',
-          },
-        ],
-      },
 };
+
+export default nextConfig;
