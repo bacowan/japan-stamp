42a889b5083277a933d6364815255d0a30addce5
bacowan <cowan.brendan.a@gmail.com>
2025-02-27 23:49:46 +0900

    creating a rudimentary footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return <footer className="border-t border-border text-primary flex flex-row">
+        <a className="flex-grow text-center p-1">About</a>
+        <a className="flex-grow text-center p-1">Privacy Policy</a>
+        <a className="flex-grow text-center p-1">Github</a>
+    </footer>
+}
