4f15a388d1cadfef735aadd002fd084e5592ab0d
bacowan <cowan.brendan.a@gmail.com>
2025-03-07 23:57:18 +0900

    adding a privacy policy

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Footer() {
     return <footer className="border-t border-border text-primary flex flex-row">
         <a className="flex-grow text-center p-1">About</a>
-        <a className="flex-grow text-center p-1">Privacy Policy</a>
+        <Link href="/privacy-policy" className="flex-grow text-center p-1">Privacy Policy</Link>
         <a className="flex-grow text-center p-1">Github</a>
     </footer>
 }
