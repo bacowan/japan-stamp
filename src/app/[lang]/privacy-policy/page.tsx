af3d3fbfa370ecb105a87d22880a2f3e3dacc9e0
bacowan <cowan.brendan.a@gmail.com>
2025-03-09 00:00:14 +0900

    moving the privacy policy file to the public folder

--- src/app/[lang]/privacy-policy/page.tsx
+++ src/app/[lang]/privacy-policy/page.tsx
@@ -1,6 +1,7 @@
 import { promises as fs } from 'fs';
+import path from 'path';
 
 export default async function PrivacyPolicy() {
-    const text = await fs.readFile(process.cwd() + '/src/app/privacy-policy/policy.html', 'utf8');
+    const text = await fs.readFile(path.join(process.cwd(), 'public', 'privacy-policy.html'), 'utf8');
     return <div className="policy" dangerouslySetInnerHTML={{ __html: text }}/>
 }
