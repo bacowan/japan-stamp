57d2d4e91eb866a1524744b284fbc068b230746c
bacowan <cowan.brendan.a@gmail.com>
2025-03-09 00:20:44 +0900

    adding database localization

--- src/database/database_types/stamp.ts
+++ src/database/database_types/stamp.ts
@@ -1,6 +1,8 @@
+import { LocalizedText } from "./localized-text";
+
 export default interface Stamp {
-    name: string,
-    description: string,
+    name: LocalizedText,
+    description: LocalizedText,
     location: [number, number],
     image_url: string,
     last_updated: Date
--- src/localization/localization.ts
+++ src/localization/localization.ts
@@ -1,2 +1,2 @@
-export type SupportedLocale = 'en-US' | 'ja';
-export const supportedLocales: SupportedLocale[] = ['en-US', 'ja'];
+export const supportedLocales = ['en-US', 'ja'] as const;
+export type SupportedLocale = typeof supportedLocales[number];
