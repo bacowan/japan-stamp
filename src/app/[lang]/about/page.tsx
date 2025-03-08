9c0fcdb68b074e0afb06accf99850364da2e89ac
bacowan <cowan.brendan.a@gmail.com>
2025-03-09 01:11:30 +0900

    creating japanese version of about page

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,3 +1,4 @@
+import { supportedLocales } from '@/localization/localization';
 import { promises as fs } from 'fs';
 import path from 'path';
 
@@ -5,7 +6,20 @@ interface AboutPageParams {
     params: Promise<{ id: string, lang: 'en-US' | 'ja' }>
 }
 
-export default async function About() {
-    const text = await fs.readFile(path.join(process.cwd(), 'public', 'about-en-US.html'), 'utf8');
+export default async function About({ params }: AboutPageParams) {
+    const resolvedParams = await params;
+    const aboutFileName = `about-${resolvedParams.lang}.html`;
+    const aboutFilePath = path.join(process.cwd(), 'public', aboutFileName);
+
+    let text: string;
+    try {
+        text = await fs.readFile(aboutFilePath, 'utf8');
+    }
+    catch {
+        const defaultAboutFileName = `about-${supportedLocales[0]}.html`;
+        const defaultAboutFilePath = path.join(process.cwd(), 'public', defaultAboutFileName);
+        text = await fs.readFile(defaultAboutFilePath, 'utf8');
+    }
+    
     return <div className="about" dangerouslySetInnerHTML={{ __html: text }}/>
 }
