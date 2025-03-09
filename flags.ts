e954633933a781ea45facc84f4aa19a64aa4069a
bacowan <cowan.brendan.a@gmail.com>
2025-03-09 21:05:03 +0900

    setting environment variables for default flag values

--- flags.ts
+++ flags.ts
@@ -3,13 +3,6 @@ import { flag } from 'flags/next';
 export const stampListPageFlag = flag({
   key: 'stamp-list-page',
   decide() {
-    return false;
-  },
-});
-
-export const forceJpFlag = flag({
-  key: 'force-jp',
-  decide() {
-    return false;
+    return !!process.env.FEATURE_FLAG_STAMPS_PAGE;
   },
 });
