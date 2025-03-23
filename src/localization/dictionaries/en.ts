60ece9d0dc0afc816db7bd9b2b12a1a7670d1460
bacowan <cowan.brendan.a@gmail.com>
2025-03-23 23:27:11 +0900

    updating privacy policy for EU users

--- src/localization/dictionaries/en.ts
+++ src/localization/dictionaries/en.ts
@@ -34,7 +34,7 @@ export default {
         "intro": [
             "ekistamp.com is committed to protecting your privacy and ensuring a safe online experience. You can turn of various privacy related settings here, or at the Privacy Preferences page linked at the bottom of the screen. By using this website you also agree to our ",
             { "type": "link", "href": "/privacy-policy", "value": "full privacy policy" },
-            "."
+            ". Please note that for users in European Union countries, we do not collect or store any data other than that which is required for the website to function."
         ],
         "required-storage": "Required Storage",
         "required-storage-body": "ekistamp.net uses local storage on your computer for the following basic functionality of the website which cannot be opted out of:",
