9d213bdd3f81d71e8325bbe7e3ec4a982e5886bb
bacowan <cowan.brendan.a@gmail.com>
2025-03-24 22:30:07 +0900

    Reapply "50 create modal for explicit user permissions (#53)"
    This reverts commit cedec67572e3ad7728f7c96d3dd2fbf7c114acb6.

--- src/app/[lang]/utils/localize-href.ts
+++ src/app/[lang]/utils/localize-href.ts
@@ -1,5 +1,6 @@
 import { SupportedLocale } from "@/localization/localization";
+import { Url } from "next/dist/shared/lib/router/router";
 
-export default function localizeHref(locale: SupportedLocale, href: string) {
+export default function localizeHref(locale: SupportedLocale, href: Url) {
     return `/${locale}${href}`;
 }
